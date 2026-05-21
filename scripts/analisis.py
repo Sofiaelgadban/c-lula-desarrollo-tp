2eddf74038a0077032cc468eb95ffbd51e593f85
Sofia Elgadban <sofisanchezgusmeroli@hotmail.com>
2026-05-21 17:19:31 -0300

    PROY-2: desarrollo del análisis de ventas

--- scripts/analisis.py
+++ scripts/analisis.py
@@ -1,1 +1,18 @@
+with open("datos/dataset.csv", "r") as archivo:
+    lineas = archivo.readlines()
 
+total_ventas = 0
+
+for linea in lineas[1:]:
+    datos = linea.strip().split(",")
+
+    producto = datos[0]
+    cantidad = int(datos[1])
+    precio = int(datos[2])
+
+    total_ventas += cantidad * precio
+
+print("Total de ventas:", total_ventas)
+
+with open("resultados/resultado.txt", "w") as resultado:
+    resultado.write(f"Total de ventas: {total_ventas}")
